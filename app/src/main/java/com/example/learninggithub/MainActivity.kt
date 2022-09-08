176db6f0942d33c824c3ca2d559403f245ae33fe
Justin Albert <65417105+jnalbert@users.noreply.github.com>
2022-09-08 09:30:40 -0700

    Update MainActivity.kt
    Added another important variable

--- app/src/main/java/com/example/learninggithub/MainActivity.kt
+++ app/src/main/java/com/example/learninggithub/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // very important variable
         val x = 10
+        val work = "IDK"
     }
 
 
